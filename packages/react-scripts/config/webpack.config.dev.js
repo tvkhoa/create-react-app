27c45e0de1c7544e2e251ee41bbb5769f3863eee
Nguyen Nguyen <nguyen.ntph@gmail.com>
2019-05-15 11:07:52 +0700

    Add hard source

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -406,6 +406,8 @@ module.exports = {
       inject: true,
       template: paths.appHtml,
     }),
+    // Hard source plugin for cahching
+    new HardSourceWebpackPlugin(),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
