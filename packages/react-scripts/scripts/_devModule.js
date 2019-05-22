5ea955264de403ad7c20426642eba20a9d92127c
Khoa Thai <khoathaiviet@gmail.com>
2019-05-22 22:39:13 +0700

    Change start module script

--- packages/react-scripts/scripts/_devModule.js
+++ packages/react-scripts/scripts/_devModule.js
@@ -1,16 +1,13 @@
 import { Intl } from 'redux-intl';
-import createHistory from 'history/createBrowserHistory';
 
 import '../src/initializer/intlPolyfill';
 import '../src/initializer';
-import { configureStore } from '../src/state/createStore';
+import { configureStore } from './intlStore';
 
 // @replace-import-on-dev-start-begin
 // @replace-import-on-dev-start-end
 
-const history = createHistory();
-
-const store = configureStore(undefined, history);
+const store = configureStore();
 Intl.setStore(store);
 
 export default [
