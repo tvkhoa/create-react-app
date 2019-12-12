29cb345ef2394b7aed703111621323c61b6dc0e9
Vu PHAM <vu.pham@employmenthero.com>
2019-12-12 16:38:19 +0700

    Disable parallel of terser plugin

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -223,6 +223,7 @@ module.exports = function(webpackEnv) {
         // This is only used in production mode
         new TerserPlugin({
           terserOptions: {
+            parallel: false,
             parse: {
               // We want terser to parse ecma 8 code. However, we don't want it
               // to apply any minification steps that turns valid ecma 5 code
